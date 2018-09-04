600523b3624c6f36d645fe052210e712db97a7b5
Qing Tian <qingtian@lawn-143-215-58-240.lawn.gatech.edu>
2018-09-04 11:49:15 -0400

    Adding tabs for signin and register, added register page

--- src/views/initialLogin.vue
+++ src/views/initialLogin.vue
@@ -1,41 +1,102 @@
 <template>
     <v-container>
-        <v-layout align-center justify-center>
-            <v-flex xs6>
-                <v-form ref="form" v-model="valid" lazy-validation>
-                    <v-text-field
-                            v-model="email"
-                            label="email"
-                            required
-                    ></v-text-field>
-                    <v-text-field
-                            v-model="password"
-                            label="password"
-                            required
-                    ></v-text-field>
-                    <v-layout align-center justify-center>
-                    <v-flex sm12 style="text-align: center">
-                    <v-btn
-                            :disabled="!valid"
-                            @click="submit"
-                            color="info"
-                    >
-                        Sign in
-                    </v-btn>
+        <v-tabs
+                b-model="active"
+                color="info"
+                slider-color="yellow">
+            <v-tab
+                    v-for="i in tabs"
+                    :key="i"
+                    ripple
+            class="white--text">
+                {{i}}
+            </v-tab>
+            <v-tab-item
+                    v-for="i in tabs"
+                    :key="i"
+                    ripple>
+                <v-layout align-center justify-center v-if="i === 'signin'">
+                    <v-flex xs6>
+                        <v-form ref="form" v-model="valid" lazy-validation>
+                            <v-text-field
+                                    v-model="email"
+                                    label="email"
+                                    required
+                            ></v-text-field>
+                            <v-text-field
+                                    v-model="password"
+                                    label="password"
+                                    required
+                            ></v-text-field>
+                            <v-layout align-center justify-center>
+                                <v-flex sm12 style="text-align: center">
+                                    <v-btn
+                                            :disabled="!valid"
+                                            @click="submit"
+                                            color="info"
+                                    >
+                                        Google Sign in
+                                    </v-btn>
+
+                                    <v-btn
+                                            :disabled="!valid"
+                                            @click="submit"
+                                            color="info"
+                                    >
+                                        Facebook sign in
+                                    </v-btn>
+                                </v-flex>
+                            </v-layout>
+                        </v-form>
                     </v-flex>
-                    </v-layout>
-                </v-form>
-            </v-flex>
-        </v-layout>
+                </v-layout>
+
+                <v-layout align-center justify-center v-if="i === 'register'">
+                    <v-flex xs6>
+                        <v-form ref="form" v-model="valid" lazy-validation>
+                            <v-text-field
+                                    v-model="email"
+                                    label="email"
+                                    required
+                            ></v-text-field>
+                            <v-text-field
+                                    v-model="password"
+                                    label="password"
+                                    required
+                            ></v-text-field>
+                            <v-layout align-center justify-center>
+                                <v-flex sm12 style="text-align: center">
+                                    <v-btn
+                                            :disabled="!valid"
+                                            @click="submit"
+                                            color="info"
+                                    >
+                                        Register
+                                    </v-btn>
+
+                                </v-flex>
+                            </v-layout>
+                        </v-form>
+                    </v-flex>
+                </v-layout>
+            </v-tab-item>
+        </v-tabs>
     </v-container>
 
 </template>
 
 <script>
     import axios from 'axios'
+    import VTabs from "vuetify/src/components/VTabs/VTabs";
+    import VTab from "vuetify/src/components/VTabs/VTab";
+    import VTabItem from "vuetify/src/components/VTabs/VTabItem";
 
     export default {
+        components: {VTabItem, VTab, VTabs},
         data: () => ({
+            active: 0,
+            tabs: ["signin", "register"],
+            password: "",
             valid: true,
             name: '',
             nameRules: [
